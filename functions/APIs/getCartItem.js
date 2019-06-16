0636b1dbf5c27f9edbd181d7a92118a47a76b6ca
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:15:58 -0400

    Update getCartItem.js

--- functions/APIs/getCartItem.js
+++ functions/APIs/getCartItem.js
@@ -3,21 +3,13 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// GetCartItem - Get information for a specific item in a customer's cart 
+
 exports.handler = (event, context, callback) => {
   
-  // Return immediately if being called by warmer 
-  if (event.source === "warmer") {
-    return callback(null, "Lambda is warm");
-  }
+  
 
   const params = {
-    TableName: process.env.TABLE_NAME, // [ProjectName]-Cart
-    // 'Key' defines the partition and sort keys of the item to be retrieved
-    // - 'customerId': user identities are federated through the
-    //                 Cognito Identity Pool, we will use the identity id
-    //                 as the user id of the authenticated user
-    // - 'bookId': a unique identifier for the book (uuid)
+    TableName: process.env.TABLE_NAME, 
     Key: {
       customerId: event.requestContext.identity.cognitoIdentityId,
       bookId: event.pathParameters.bookId
@@ -25,13 +17,13 @@ exports.handler = (event, context, callback) => {
   };
 
   dynamoDb.get(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials" : true
     };
 
-    // Return status code 500 on error
+    
     if (error) {
       const response = {
         statusCode: 500,
@@ -42,7 +34,7 @@ exports.handler = (event, context, callback) => {
       return;
     }
 
-    // Return status code 200 and the retrieved item on success
+    
     const response = {
       statusCode: 200,
       headers: headers,
