f6af95de1829a7acf1674abd4249d4aa81aa2c55
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:22:01 -0400

    Update updateCart.js

--- functions/APIs/updateCart.js
+++ functions/APIs/updateCart.js
@@ -3,28 +3,19 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// UpdateCart - Update quantity of a book in a customer's cart
+
 exports.handler = (event, context, callback) => {
   
-  // Return immediately if being called by warmer 
-  if (event.source === "warmer") {
-    return callback(null, "Lambda is warm");
-  }
-  
-  // Request body is passed in as a JSON encoded string in 'event.body'
+ 
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.TABLE_NAME,
-    // 'Key' defines the partition and sort keys of the item to be updated
-    // - 'customerId': Identity Pool identity id of the authenticated user
-    // - 'bookId': id for the book being updated
+   
     Key: {
       customerId: event.requestContext.identity.cognitoIdentityId,
       bookId: data.bookId
     },
-    // 'UpdateExpression' defines the attributes to be updated
-    // 'ExpressionAttributeValues' defines the value in the update expression
-    // - ':quantity': defines 'quantity' to be the quantity parsed from the request body
+    
     UpdateExpression: "SET quantity = :quantity",
     ExpressionAttributeValues: {
       ":quantity": data.quantity
@@ -38,7 +29,7 @@ exports.handler = (event, context, callback) => {
       "Access-Control-Allow-Credentials" : true
     };
 
-    // Return status code 500 on error
+    
     if (error) {
       const response = {
         statusCode: 500,
@@ -49,7 +40,7 @@ exports.handler = (event, context, callback) => {
       return;
     }
 
-    // Return status code 200 on success
+    
     const response = {
       statusCode: 200,
       headers: headers
