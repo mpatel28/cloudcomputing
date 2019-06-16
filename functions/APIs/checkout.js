800a230e089fcd93b63a40ac5c4b773c75e6ec77
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:12:52 -0400

    Update checkout.js

--- functions/APIs/checkout.js
+++ functions/APIs/checkout.js
@@ -4,25 +4,14 @@ const AWS = require("aws-sdk");
 const uuid = require("uuid");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// Checkout - Process a customer's cart into an order
+
 exports.handler = (event, context, callback) => {
-    
-  // Return immediately if being called by warmer 
-  if (event.source === "warmer") {
-    return callback(null, "Lambda is warm");
-  }
+
   
-  // Request body is passed in as a JSON encoded string in 'event.body'
+  
   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.ORDERS_TABLE, // [ProjectName]-Orders
-    // 'Item' contains the attributes of the item to be created
-    // - 'customerId': user identities are federated through the
-    //                 Cognito Identity Pool, we will use the identity id
-    //                 as the user id of the authenticated user
-    // - 'orderId': a unique uuid
-    // - 'orderDate': current date
-    // - 'books': list of books parsed from request body
+    TableName: process.env.ORDERS_TABLE, 
     Item: {
       customerId: event.requestContext.identity.cognitoIdentityId,
       orderId: uuid.v1(),
@@ -32,13 +21,13 @@ exports.handler = (event, context, callback) => {
   };
  
   dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials" : true
     };
 
-    // Return status code 500 on error
+  
     if (error) {
       const response = {
         statusCode: 500,
@@ -49,7 +38,7 @@ exports.handler = (event, context, callback) => {
       return;
     }
 
-    // Return status code 200 on success
+    
     const response = {
       statusCode: 200,
       headers: headers
@@ -57,16 +46,11 @@ exports.handler = (event, context, callback) => {
     callback(null, response);
   });
  
-  // Delete books from cart as they have been processed
+  
   let deleteParam = {}
   data.books.forEach( function(item) {
     deleteParam = {
-      TableName: process.env.CART_TABLE, // [ProjectName]-Cart
-      // 'Key' defines the partition key and sort key of the item to be deleted
-      // - 'customerId': user identities are federated through the
-      //                 Cognito Identity Pool, we will use the identity id
-      //                 as the user id of the authenticated user
-      // - 'bookId': a unique identifier for the book (uuid)
+      TableName: process.env.CART_TABLE,
       Key: {
         customerId: event.requestContext.identity.cognitoIdentityId,
         bookId: item.bookId
@@ -74,13 +58,13 @@ exports.handler = (event, context, callback) => {
     }
  
     dynamoDb.delete(deleteParam, (error, data) => {
-      // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+     
       const headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials" : true
       };
 
-      // Return status code 500 on error
+      
       if (error) {
         const response = {
           statusCode: 500,
@@ -91,7 +75,7 @@ exports.handler = (event, context, callback) => {
         return;
       }
 
-      // Return status code 200 on success
+      
       const response = {
         statusCode: 200,
         headers: headers
