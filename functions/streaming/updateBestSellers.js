2fb9d407530b4e1dfce3cab28c4e7cc1c50130ea
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:23:49 -0400

    Update updateBestSellers.js

--- functions/streaming/updateBestSellers.js
+++ functions/streaming/updateBestSellers.js
@@ -2,11 +2,11 @@
 
 var redis = require("redis");
 
-// UpdateBestSellers - Updates best sellers list as orders are placed
+
 exports.handler = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  var redisClient = redis.createClient(6379, process.env.URL, {no_ready_check: true}); // ElastiCache Redis cluster URL
+  var redisClient = redis.createClient(6379, process.env.URL, {no_ready_check: true}); 
   console.log("Client created.");
 
   redisClient.on("error", function (err) {
@@ -27,8 +27,7 @@ exports.handler = (event, context, callback) => {
       var value = book.M.bookId.S; // bookId
       var key = "TopBooks:AllTime";
 
-      // Increment the score of the member (bookId) in the sorted set stored at key (TopBooks:AllTime) by increment (itemsSold)
-      // If the bookId does not exist in the sorted set, it is added with increment as its score
+      
       redisClient.zincrby(key, itemsSold, JSON.stringify(value), (error, reply) => {
         if(error) {
           console.log("error: " + error);
