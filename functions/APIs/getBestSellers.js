6bc9820cb0486963199a4952832bec79032fcafa
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:13:54 -0400

    Update getBestSellers.js

--- functions/APIs/getBestSellers.js
+++ functions/APIs/getBestSellers.js
@@ -2,12 +2,12 @@
 
 const redis = require("redis");
 
-// GetBestSellers - Get a list of the top 20 best selling books
+
 exports.handler = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
     
     console.log("Creating client.");
-    var redisClient = redis.createClient(6379, process.env.URL, {no_ready_check: true}); // URL of Redis cluster
+    var redisClient = redis.createClient(6379, process.env.URL, {no_ready_check: true}); 
     console.log("Client created.");
     
     redisClient.on("error", function (err) {
@@ -20,16 +20,15 @@ exports.handler = (event, context, callback) => {
     
     var key = "TopBooks:AllTime"
 
-    // Return the first 20 elements in the sorted set stored at key 'TopBooks:AllTime'
-    // The elements are ordered from the highest to lowest scores
+    
     redisClient.zrevrange(key, 0, 19, (error, members) => {
-      // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+      
       const headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials" : true
       };
       
-      // Return status code 500 on error
+      
       if (error) {
         const response = {
            statusCode: 500,
@@ -40,7 +39,7 @@ exports.handler = (event, context, callback) => {
         return;
       }
       
-      // Return status code 200 and the top books on success
+      
       const response = {
         statusCode: 200,
         headers: headers,
