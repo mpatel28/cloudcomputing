9d4b0581e392b8eae2c7b606379d573ba8f84a60
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:18:58 -0400

    Update listItemsInCart.js

--- functions/APIs/listItemsInCart.js
+++ functions/APIs/listItemsInCart.js
@@ -3,22 +3,13 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// ListItemsInCart - List all items in a customer's cart
+
 exports.handler = (event, context, callback) => {
   
-  // Return immediately if being called by warmer 
-  if (event.source === "warmer") {
-    return callback(null, "Lambda is warm");
-  }
+  
 
   const params = {
-    TableName: process.env.TABLE_NAME, // [ProjectName]-Cart
-    // 'KeyConditionExpression' defines the condition for the query
-    // - 'customerId = :customerId': only return items with matching 'customerId'
-    //                               partition key
-    // 'ExpressionAttributeValues' defines the value in the condition
-    // - ':customerId': defines 'customerId' to be Identity Pool identity id
-    //                  of the authenticated user
+    TableName: process.env.TABLE_NAME, 
     KeyConditionExpression: "customerId = :customerId",
     ExpressionAttributeValues: {
       ":customerId": event.requestContext.identity.cognitoIdentityId
@@ -26,13 +17,13 @@ exports.handler = (event, context, callback) => {
   };
 
   dynamoDb.query(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials" : true
     };
 
-    // Return status code 500 on error
+    
     if (error) {
       const response = {
         statusCode: 500,
@@ -43,7 +34,7 @@ exports.handler = (event, context, callback) => {
       return;
    }
 
-    // Return status code 200 and the retrieved items on success
+    
     const response = {
       statusCode: 200,
       headers: headers,
