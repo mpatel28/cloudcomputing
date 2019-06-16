b303f1dd2f65ed7aebceb9a939bd9359411b0983
mpatel28 <44143566+mpatel28@users.noreply.github.com>
2019-06-16 15:11:39 -0400

    Update addToCart.js

--- functions/APIs/addToCart.js
+++ functions/APIs/addToCart.js
@@ -3,25 +3,12 @@
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// AddToCart - add books to a customer's cart
-exports.handler = (event, context, callback) => {
-  
-  // Return immediately if being called by warmer  
-  if (event.source === "warmer") {
-    return callback(null, "Lambda is warm");
-  }
 
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  const data = JSON.parse(event.body);
+exports.handler = (event, context, callback) => {
+   const data = JSON.parse(event.body);
   const params = {
-    TableName: process.env.TABLE_NAME, // [ProjectName]-Cart
-    // 'Item' contains the attributes of the item to be created
-    // - 'customerId': user identities are federated through the
-    //                 Cognito Identity Pool, we will use the identity id
-    //                 as the user id of the authenticated user
-    // - 'bookId': a unique identifier for the book (uuid)
-    // - 'quantity': the number of 
-    // - 'price': price of the 
+    TableName: process.env.TABLE_NAME, 
+    
     Item: {
       customerId: event.requestContext.identity.cognitoIdentityId,
       bookId: data.bookId,
@@ -31,13 +18,13 @@ exports.handler = (event, context, callback) => {
   };
 
   dynamoDb.put(params, (error, data) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials" : true
     };
 
-    // Return status code 500 on error
+    
     if (error) {
       const response = {
         statusCode: 500,
@@ -48,7 +35,7 @@ exports.handler = (event, context, callback) => {
       return;
     }
 
-    // Return status code 200 on success
+   
     const response = {
       statusCode: 200,
       headers: headers
